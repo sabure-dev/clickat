388ac10f410ca8128a1790a8e427c4e6c5ff683f
sabure <dev.sabure@gmail.com>
2024-06-18 16:47:21 +0300

    deploying test url changed

--- backend/src/database.py
+++ backend/src/database.py
@@ -10,7 +10,7 @@ class Base(DeclarativeBase):
     pass
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgres://dragon:eBTHjighD22dEMZf9h93F8JLvZTNf045@dpg-cpooihaju9rs738stiig-a/clickat"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
